211f9aba3747788e0925b36e7f3a04345dc713da
arynnmp <126384299+arynnmp@users.noreply.github.com>
2023-09-02 02:12:52 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,9 @@
+#Arynn Martin-Post
+#I am tall
+print("   A   M   M ")
+print("  A A  MM MM ")
+print(" A A A MM MM ")
+print(" AAAAA M M M ")
+print(" A   A M   M ")
+print(" A   A M   M ")
+print(" A   A M   M ")
